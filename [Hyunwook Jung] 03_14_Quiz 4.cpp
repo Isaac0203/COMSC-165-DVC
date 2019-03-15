a503c8347810401f138fcbaad8e2af16d66dfd9f
Isaac0203 <47236101+Isaac0203@users.noreply.github.com>
2019-03-14 21:44:52 -0700

    Add files via upload

---
+++ [Hyunwook Jung] 03_14_Quiz 4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+// Hyunwook Jung DVC
+// 03 14 2019 COMSC 165
+// QUIZ #4
+
+using namespace std;
+int main()
+{
+	string a;
+	cout << "Enter a string: ";
+	cin >> a;
+
+	// Sort!! in reverse order.
+	int largest = a[0];
+	int largestIndex = 0;
+	int aLength = a.length();
+	cout << endl << aLength;
+	int temp;	// For swapping values
+
+	for (int count1 = 0; count1 < aLength; ++count1)
+	{
+		largest = a[count1];
+
+		for (int count2 = count1; count2 < aLength; count2++)
+		{
+
+			if (largest < a[count2])
+			{
+				largest = a[count2];
+				largestIndex = count2;
+			}
+		}
+		// After finding the largest value, swap the value of a[count1] <> a[largest]
+		temp = a[count1];
+		a[count1] = a[largestIndex];
+		a[largestIndex] = temp;
+	}
+
+	cout << endl << a;
+	return 0;
+}
